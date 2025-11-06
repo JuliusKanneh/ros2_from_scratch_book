1362a9a6052481200df2fb126953cca56f0428b8
JuliusKanneh <jkanneh@andrew.cmu.edu>
2025-11-06 02:32:54 +0200

    Creating custom Interface(Message) for topic

--- build/angular_vel/CMakeFiles/angular_vel_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/angular_vel_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/angular_vel_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
